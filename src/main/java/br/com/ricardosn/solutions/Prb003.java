ff8babb275dadaee96d1b36542bbbbd247cf72e7
ricardosn <ricardorichsn@gmail.com>
2021-07-04 10:43:54 -0300

    Solution to problem 3. closes #5.

--- src/main/java/br/com/ricardosn/solutions/Prb003.java
+++ src/main/java/br/com/ricardosn/solutions/Prb003.java
@@ -9,32 +9,36 @@ public final class Prb003 {
         System.out.println(new Prb003().run());
     }
 
+    /*
+    * O conceito matemático utilizado foi o de decomposição em fatores primos por divisões sucessivas
+    * O teorema diz que um número inteiro n pode ser decomposto pela multiplicação de m números primos, sendo que
+    * pm > 1:
+    * n = p0 * p1 * p2 * ... * pm
+    */
     public long run() {
+        long n = 600851475143L;
 
-        long largestPrime = 0L;
-
-        for(long i = 1; i <= 13195L; i++) {
-
-            int count = 0;
-
-            for(int j = 1; j <= i; j++) {
-
-                if(i%j == 0) {
-                    count++;
-                }
-
-                if (count > 2) {
-                    break;
-                }
-            }
-
-            if(count <= 2) {
-                System.out.print(i + " - ");
-                largestPrime = i;
-            }
+        while(true) {
+            long p = smallestPrimeFactor(n);
+            if (p < n)
+                n /= p;
+            else
+                return n;
         }
+    }
 
-        return largestPrime;
+    private static long smallestPrimeFactor(long n) {
+        if(n <= 1)
+            throw new IllegalArgumentException();
+
+        // Não é necessário testar [2, n], pois a raiz quadrada é o resultado da multiplicação de todos os fatores primos
+        int end= (int) Math.sqrt(n);
+
+        for(int i = 2; i <= end; i++) {
+            if(n % i == 0)
+                return i;
+        }
+        return n;
     }
 
 }
